647b7935239269ad3232a88ca74633459275af87
Maxim Volgin <maxvog2020@gmail.com>
2023-05-05 23:48:02 +0300

    Move test and extract "print_sum_code" function

--- src/codegen/code_gen.rs
+++ src/codegen/code_gen.rs
@@ -52,3 +52,25 @@ impl<'ctx> JitCompileSum<'ctx> for CodeGen<'ctx> {
     }
 }
 
+#[test]
+fn test_sum_func_call() {
+    let context = Context::create();
+    let codegen = CodeGen::new(&context, OptimizationLevel::Aggressive);
+    let sum = codegen.jit_compile_sum().unwrap();
+
+    let collections = [
+        (1, 2, 3),
+        (2, 3, 4),
+        (3, 4, 5),
+        (4, 5, 6),
+        (7, 5, 2),
+        (8, 5, 3),
+        (9, 5, 4),
+        (10, 5, 5),
+    ];
+
+    for (x, y, z) in collections {
+        unsafe { assert_eq!(sum.call(x, y, z), x + y + z); }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,17 @@ use codegen::JitCompileSum;
 use inkwell::context::Context;
 use inkwell::OptimizationLevel;
 
-fn main() {
+fn print_sum_code(opt_level: OptimizationLevel) {
     let context = Context::create();
-    let codegen = CodeGen::new(&context, OptimizationLevel::Aggressive);
+    let codegen = CodeGen::new(&context, opt_level);
+
     codegen.jit_compile_sum();
-    codegen.module.print_to_stderr();
+    print!("{}\n\n", codegen.module.print_to_string().to_string());
 }
 
-#[test]
-fn test_sum_func_call() {
-    let context = Context::create();
-    let codegen = CodeGen::new(&context, OptimizationLevel::Aggressive);
-    let sum = codegen.jit_compile_sum().unwrap();
-
-    let collections = [
-        (1, 2, 3),
-        (2, 3, 4),
-        (3, 4, 5),
-        (4, 5, 6),
-        (7, 5, 2),
-        (8, 5, 3),
-        (9, 5, 4),
-        (10, 5, 5),
-    ];
-
-    for (x, y, z) in collections {
-        unsafe { assert_eq!(sum.call(x, y, z), x + y + z); }
-    }
+fn main() {
+    print_sum_code(OptimizationLevel::None);
+    print_sum_code(OptimizationLevel::Less);
+    print_sum_code(OptimizationLevel::Aggressive);
 }
+
